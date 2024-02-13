7bdb4f51c169585c537f393c91d5084fe57953c2
subhadeepchowdhury41 <subhadeepchowdhury41@gmail.com>
2024-02-13 19:08:07 +0530

    meassges

---
+++ src/data/constants.js
@@ -0,0 +1,303 @@
+export const Bio = {
+  name: "Subhadeep Chowdhury",
+  roles: [
+    "Full-Stack Developer",
+    "Backend Developer",
+    "Mobile Developer",
+    "Frontend  Developer",
+    "DevOps Engineer",
+    "ML/AI Engineer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/subhadeepchowdhury41",
+  resume:
+    "https://drive.google.com/file/d/1eEhK8DKB8vYNj_sbkzyIrGXze8UrBK0R/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/subhadeep-chowdhury-4a54a0205/",
+  twitter: "https://twitter.com/subhadeepchowd6",
+  insta: "https://www.instagram.com/subhadeep9809/",
+  facebook: "https://www.facebook.com/subha.chowdhury.108",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: 'Tailwind CSS',
+        image: 'https://seeklogo.com/images/T/tailwind-css-logo-5AD4175897-seeklogo.com.png'
+      },
+      {
+        name: 'Material UI',
+        image: 'https://mui.com/static/logo.png'
+      },
+      {
+        name: 'Redux',
+        image: 'https://cdn.worldvectorlogo.com/logos/redux.svg'
+      },
+      {
+        name: 'Svelte',
+        image: 'https://miro.medium.com/v2/resize:fit:400/0*r6OTkIa8jyneCBGY.png'
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image:
+          "https://seeklogo.com/images/N/nodejs-logo-54107C5EDD-seeklogo.com.png?v=638179441380000000",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://banner2.cleanpng.com/20180425/jrw/kisspng-node-js-javascript-web-application-express-js-comp-5ae0f84e2a4242.1423638015246930701731.jpg",
+      },
+      {
+        name: "Django",
+        image:
+          "https://static-00.iconduck.com/assets.00/django-icon-402x512-tkdfpj8s.png",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://seeklogo.com/images/M/mongodb-logo-655F7D542D-seeklogo.com.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-images-are-download-crazypng-21.png",
+      },
+      {
+        name: "PostgreSQL",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1280px-Postgresql_elephant.svg.png",
+      },
+      {
+        name: "Firebase",
+        image:
+          "https://e7.pngegg.com/pngimages/331/583/png-clipart-firebase-computer-icons-mobile-backend-as-a-service-mobile-app-google-cloud-angle-cloud.png",
+      },
+      {
+        name: "GraphQL",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/1280px-GraphQL_Logo.svg.png",
+      },
+      {
+        name: "REST API",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Web-API.svg/1280px-Web-API.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Mobile",
+    skills: [
+      {
+        name: "React Native",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://seeklogo.com/images/F/flutter-logo-5086DD11C5-seeklogo.com.png"
+      },
+      {
+        name: "Dart",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png",
+      },
+      {
+        name: 'Jetpack Compose',
+        image: 'https://developer.android.com/images/brand/Android_Robot.png'
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://seeklogo.com/images/K/kotlin-logo-30C1970B05-seeklogo.com.png",
+      },
+    ]
+  },
+  {
+    title: "Tools",
+    skills: [
+      {
+        name: "VS Code",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/visual-studio-code-icon.png",
+      },
+      {
+        name: "Visual Studio",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Visual_Studio_Icon_2019.svg/2060px-Visual_Studio_Icon_2019.svg.png"
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Android_Studio_icon_%282023%29.svg/2048px-Android_Studio_icon_%282023%29.svg.png",
+      },
+      {
+        name: 'Xcode',
+        image: 'https://upload.wikimedia.org/wikipedia/en/5/56/Xcode_14_icon.png'
+      },
+      {
+        name: "Microsoft Office",
+        image:
+          "https://logowik.com/content/uploads/images/microsoft-office3327.jpg",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+      },
+      {
+        name: "Figma",
+        image: "https://cdn-icons-png.flaticon.com/512/5968/5968705.png"
+      }
+    ]
+  },
+  {
+    title: "DevOps and Cloud",
+    skills: [
+      {
+        name: "Git and GitHub",
+        image:
+          "https://seeklogo.com/images/G/github-logo-5F384D0265-seeklogo.com.png",
+      },
+      {
+        name: "GitLab",
+        image: "https://seeklogo.com/images/G/gitlab-logo-757620E430-seeklogo.com.png"
+      },
+      {
+        name: "Docker",
+        image:
+          "https://seeklogo.com/images/D/docker-logo-CF97D0124B-seeklogo.com.png",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://seeklogo.com/images/K/kubernetes-logo-3A67038EAB-seeklogo.com.png",
+      },
+      {
+        name: "AWS",
+        image:
+          "https://seeklogo.com/images/A/aws-logo-9B1A5B8193-seeklogo.com.png",
+      },
+      {
+        name: "GCP",
+        image:
+          "https://seeklogo.com/images/G/google-cloud-logo-ADE788217F-seeklogo.com.png",
+      },
+      {
+        name: "Azure",
+        image:
+          "https://seeklogo.com/images/M/microsoft-azure-logo-91B2BCF6CE-seeklogo.com.png",
+      },
+    ]
+  }
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "",
+    role: "Full-Stack Developer",
+    company: "Reignsys Softech Private Limited",
+    date: "Feb 2022 - Aug 2023",
+    desc: "Designed and developed SaaS Application.",
+    skills: [
+      "React.js",
+      "Firebase",
+      "Flutter",
+      "Django",
+      "Redux.js",
+      "MySQL",
+      "GCP",
+    ],
+  },
+  {
+    id: 1,
+    img: "",
+    role: "Mobile Developer",
+    company: "Inindia Pvt. Ltd.",
+    date: "Mar 2022 - Aug 2023",
+    desc: "Designed and developed Mobile Application using Flutter.",
+    skills: [
+      "Firebase",
+      "Flutter",
+      "Postman",
+      "REST API",
+      "Provider",
+      "Hive",
+      "GetX"
+    ],
+  },
+  {
+    id: 2,
+    img: "",
+    role: "Full-stack Mobile Developer",
+    company: "Zenifin Technologies Pvt Ltd",
+    date: "Feb 2023 – June 2023",
+    desc: "Integrated Bank Account Integration in Mobile Application using AAA API",
+    skills: [
+      "Flutter",
+      "WebSockets",
+      "WebHooks",
+      "Node.js",
+      "Bcrypt.js"
+    ]
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUi5HRuZ8EUlhYEQ5jxHZTadFbt7nLzuJDjw&usqp=CAU",
+    school: "Indian Institute of Information Technology, Kalyani",
+    date: "Dec 2020 - July 2024",
+    grade: "7.5 CGPA",
+    desc: "I am parsuing my  Bachelor's degree in Computer Science and Engineering at Indian Institute of Information Technology, Kalyani with a CGPA  of 7.5. ",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQV_5gpPnN_jV00RUnd5TuOiWhvqtzOBMxh0FxbSfSIUX_xCTg780JtsQjMklmkFupPc94&usqp=CAU",
+    school: "RKMV Narendrapur",
+    date: "Aug 2018 - Jun 2020",
+    grade: "93.4%",
+    desc: "I completed my class 12th education at RKMV Narendrapur, Kadma, where I studied General Science.",
+    degree: "WBBHSE(X), General Science",
+  },
+];
+
+export const projects = [];
+
+export const TimeLineData = [];
